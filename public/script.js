adf65ad310c2ae2c2c658e4eaaa8ed2e787cec08
Glitch (hello-express) <none>
2020-04-24 11:29:39 +0000

    🕷🎭 Checkpoint ./public/script.js:562062/5876

--- public/script.js
+++ public/script.js
@@ -1,7 +1,19 @@
  console.log("Oh hai! 🖤");
 
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}    
+
+docReady(function() {
   const resultsDiv = document.getElementById("history");
 
+  console.log(resultsDiv);
   async function connect() {
     const ably = new Ably.Realtime.Promise({ authUrl: '/api/createTokenRequest' });
     const channelId = "tshirt";
@@ -21,8 +33,9 @@
     for (const item of resultPage.items) {
       const result = document.createElement("div");
       result.classList.add("item");
-      result.innerHTML = JSON.stringify(item);
+      result.innerHTML = JSON.stringify(item.data);
       resultsDiv.appendChild(result);
+    }
   }
-}
-connect(); 
+  connect(); 
+});
