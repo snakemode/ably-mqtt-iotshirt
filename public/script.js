e99fee11755c99635522103be5b774502df1bc57
Glitch (hello-express) <none>
2020-04-27 17:02:57 +0000

    😋🎭 Checkpoint ./views/index.html:562062/83754:7701387/6 ./public/script.js:7701387/3412

--- public/script.js
+++ public/script.js
@@ -1,52 +1,37 @@
  console.log("Oh hai! 🖤");
 
-function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-}    
+let color = document.getElementsByClassName('color');
+let square = document.getElementsByClassName('square');
+let selected = "white";
+let pixel = 0;
+let rgb = "0,0,0"
 
-docReady(function() {
-  let color = document.getElementsByClassName('color');
-  let square = document.getElementsByClassName('square');
-  let selected = "white";
-  let pixel = 0;
-  let rgb = "0,0,0"
+for(let i = 0; i < color.length; i++) {
+  color[i].addEventListener("click", function(e){
+    selected = e.target.id;
+    rgb = e.target.dataset.rgb;
+    console.log(rgb);
+  }, false);   
+}
+
+function colorAndPublish(e, channel) {
+  pixel = e.target.id.replace('square', '');
+  e.target.style.fill = selected;
+  channel.publish("tshirt", pixel + rgb);
+}
+
+async function connect() {
+  const ably = new Ably.Realtime.Promise({ authUrl: '/api/createTokenRequest' });
+  const channelId = "tshirt";
+  const channel = await ably.channels.get(channelId);
+  await channel.attach();
   
-  for(let i = 0; i < color.length; i++) {
-    color[i].addEventListener("click", function(e){
-      selected = e.target.id;
-      rgb = e.target.dataset.rgb;
-      console.log(rgb);
-    }, false);   
+  for (let sq of square) {
+    sq.addEventListener("click", e => colorAndPublish(e, channel), false);   
   }
-    
-  async function connect() {
-    const ably = new Ably.Realtime.Promise({ authUrl: '/api/createTokenRequest' });
-    const channelId = "tshirt";
-    const channel = await ably.channels.get(channelId);
-    await channel.attach();
-    
-    for(let i = 0; i < square.length; i++) {
-      square[i].addEventListener("click", function(e){
-        pixel = e.target.id.replace('square', '');
-        e.target.style.fill = selected;
-        channel.publish("tshirt", pixel +  rgb);
 
-      }, false);   
-    }
-    
-    
-   
-    channel.subscribe(function(message) {
-      console.log(message.data);
-    }); 
-    
-    console.log("Subscribed");
-  }
-  connect(); 
-});
+  channel.subscribe(message => console.log(message.data));
+  console.log("Subscribed");
+}
+
+connect(); 
